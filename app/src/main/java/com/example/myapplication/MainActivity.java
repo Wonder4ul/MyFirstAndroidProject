f445828b28f643f8b8af754773bb9fb4b34371c7
Andrey <mr.dallas007@gmail.com>
2023-02-24 16:16:46 +0300

    Initialize resources in code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,87 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    protected static final String TAG = "MyApplication";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast.makeText( getApplicationContext(), "Initialize onCreate", Toast.LENGTH_LONG).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(TAG, "Initialize error log string");
-        Log.w(TAG, "Initialize warning log string");
-        Log.i(TAG, "Initialize information log string");
-        Log.d(TAG, "Initialize debug log string");
-        Log.v(TAG, "Initialize verbose log string");
-    }
-    @Override
-    protected void onStart() {
-        Toast.makeText( getApplicationContext(), "Initialize onStart", Toast.LENGTH_LONG).show();
-        super.onStart();
-        Log.e(TAG, "Initialize error log string");
-        Log.w(TAG, "Initialize warning log string");
-        Log.i(TAG, "Initialize information log string");
-        Log.d(TAG, "Initialize debug log string");
-        Log.v(TAG, "Initialize verbose log string");
 
-    }
-    @Override
-    protected void onResume() {
-        Toast.makeText( getApplicationContext(), "Initialize onResume", Toast.LENGTH_LONG).show();
-        super.onResume();
-        Log.e(TAG, "Initialize error log string");
-        Log.w(TAG, "Initialize warning log string");
-        Log.i(TAG, "Initialize information log string");
-        Log.d(TAG, "Initialize debug log string");
-        Log.v(TAG, "Initialize verbose log string");
-    }
-    @Override
-    protected void onPause() {
-        Toast.makeText( getApplicationContext(), "Initialize onPause", Toast.LENGTH_LONG).show();
-        super.onPause();
-        Log.e(TAG, "Initialize error log string");
-        Log.w(TAG, "Initialize warning log string");
-        Log.i(TAG, "Initialize information log string");
-        Log.d(TAG, "Initialize debug log string");
-        Log.v(TAG, "Initialize verbose log string");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText( getApplicationContext(), "Initialize onStop", Toast.LENGTH_LONG).show();
-        Log.e(TAG, "Initialize error log string");
-        Log.w(TAG, "Initialize warning log string");
-        Log.i(TAG, "Initialize information log string");
-        Log.d(TAG, "Initialize debug log string");
-        Log.v(TAG, "Initialize verbose log string");
-        if (isFinishing())
-            finish();
+        TextView text = (TextView) findViewById(R.id.wc1);
+        text.setText("Добро пожаловать в GloLib!");
 
-    }
-    @Override
-    protected void onDestroy() {
-        Toast.makeText( getApplicationContext(), "Initialize onDestroy", Toast.LENGTH_LONG).show();
-        Log.e(TAG, "Initialize error log string");
-        Log.w(TAG, "Initialize warning log string");
-        Log.i(TAG, "Initialize information log string");
-        Log.d(TAG, "Initialize debug log string");
-        Log.v(TAG, "Initialize verbose log string");
-        super.onDestroy();
-
+        ImageView image = (ImageView) findViewById(R.id.bookImage1);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.book_svgrepo_com));
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText( getApplicationContext(), "Initialize onRestart", Toast.LENGTH_LONG).show();
-        Log.e(TAG, "Initialize error log string");
-        Log.w(TAG, "Initialize warning log string");
-        Log.i(TAG, "Initialize information log string");
-        Log.d(TAG, "Initialize debug log string");
-        Log.v(TAG, "Initialize verbose log string");
-    }
 }
